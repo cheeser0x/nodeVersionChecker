53e850bdf27b0a535c834017a3b092abf5ccecb3
Sleuth <154302569+cheeser0x@users.noreply.github.com>
2024-03-02 14:58:54 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+// check-node-version.js
+
+console.log(`This platform is running Node.js version: ${process.version}`);
+
+
+const versions = process.versions;
+console.log('Full version information:');
+console.log(versions);
